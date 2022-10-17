d285e0cf3b2f8dce21158b135bb657b26eff4103
Christopher Gunn <christopherdmgunn@gmail.com>
2022-10-17 14:06:46 +0100

    ExSheet 3

--- exercises3/exercises3.hs
+++ exercises3/exercises3.hs
@@ -2,26 +2,41 @@ import Data.List
 
 {- Recursive functions on lists -}
 isAscending :: [Int] -> Bool
-isAscending _ = undefined
+isAscending [] = True
+isAscending [x] = True
+isAscending (x:y:xs) =  x < y && isAscending (y:xs)
 
 myTake :: Int -> [a] -> [a]
-myTake _ _ = undefined
+myTake 0 x = []
+myTake index (x:xs) = 
+    if index > length xs + 1 
+    then x : myTake (length xs) xs 
+    else x : myTake (index-1) xs
 
 dropOdds :: [a] -> [a]
-dropOdds _ = undefined
+dropOdds [x] = []
+dropOdds (x:xs) = evens xs
+
+evens :: [a] -> [a]
+evens [x] = [x]
+evens (x:xs) = x : dropOdds xs
 
 myIntersperse :: [a] -> a -> [a]
-myIntersperse _ _ = undefined
+myIntersperse [x] num = x : [num]
+myIntersperse (x:xs) num = x : num : myIntersperse xs num
 
 myReverseRec :: [a] -> [a]
-myReverseRec _ = undefined
+myReverseRec [] = [] 
+myReverseRec [x] = [x]
+myReverseRec (x:xs) = myReverseRec xs ++ [x]
 
 myReverseFold :: [a] -> [a]
-myReverseFold _ = undefined
+myReverseFold list = foldl (\acc elt -> elt:acc) [] list
 
 {- Higher-order functions -}
 myMap :: (a -> b) -> [a] -> [b]
-myMap _ _ = undefined
+myMap f1 [] = []
+myMap f1 (x:xs) = f1 x : myMap f1 xs
 
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
 myFoldr _ _ _ = undefined
@@ -30,7 +45,7 @@ myFoldl :: (b -> a -> b) -> b -> [a] -> b
 myFoldl _ _ _ = undefined
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter _ _ = undefined
+myFilter = undefined
 
 {- Data structures -}
 data ArithExpr =
@@ -47,13 +62,15 @@ showExpr :: ArithExpr -> String
 showExpr _ = undefined
 
 safeHead :: [a] -> Maybe a
-safeHead _ = undefined
+safeHead [] = Nothing
+safeHead(x:_) = Just x
 
 safeDiv :: Int -> Int -> Maybe Int
-safeDiv _ _ = undefined
+safeDiv i 0 = Nothing 
+safeDiv i j =  Just (i `div ` j)
 
 addSafeDiv :: (Int, Int) -> (Int, Int) -> Maybe Int
-addSafeDiv _ _ = undefined
+addSafeDiv (i,j) (k,l) = fmap sum $ sequence [safeDiv i j, safeDiv k l]
 
 data Tree a = Leaf | Node a [Tree a]
 
